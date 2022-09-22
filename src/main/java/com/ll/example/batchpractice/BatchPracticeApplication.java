445472ee7c2bdcad967d9c96629e78bdd843340e
woominsik <9737638@naver.com>
2022-09-22 09:23:48 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ll/example/batchpractice/BatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.example.batchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/example/batchpractice/BatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.example.batchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
