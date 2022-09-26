01eed335eca72d244537d81fe84d1608577b25f8
woominsik <9737638@naver.com>
2022-09-26 11:39:25 +0900

    샘플 상품옵션 8개 생성, 각 상품당 4개씩

--- src/main/java/com/ll/exam/batchpractice/app/product/service/ProductService.java
+++ src/main/java/com/ll/exam/batchpractice/app/product/service/ProductService.java
@@ -2,21 +2,30 @@ package com.ll.exam.batchpractice.app.product.service;
 
 
 import com.ll.exam.batchpractice.app.product.entity.Product;
+import com.ll.exam.batchpractice.app.product.entity.ProductOption;
 import com.ll.exam.batchpractice.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void create(String name, int price, String makerShopName) {
+    public void create(String name, int price, String makerShopName, List<ProductOption> options) {
         Product product = Product.builder()
                 .name(name)
                 .price(price)
                 .makerShopName(makerShopName)
                 .build();
+
+        productRepository.save(product);
+
+        for ( ProductOption option : options ) {
+            product.addOption(option);
+        }
 
         productRepository.save(product);
     }
