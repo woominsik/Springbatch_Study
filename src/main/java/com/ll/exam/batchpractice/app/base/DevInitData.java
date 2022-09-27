b69ab8a389808078a500ec310aa5b5e9bd912d3f
woominsik <9737638@naver.com>
2022-09-27 13:04:51 +0900

    주문을 4개까지 생성

--- src/main/java/com/ll/exam/batchpractice/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/batchpractice/app/base/DevInitData.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Profile("dev")
@@ -24,6 +25,20 @@ public class DevInitData {
     public CommandLineRunner initData(MemberService memberService, ProductService productService, CartService cartService, OrderService orderService) {
         return args ->
         {
+            class Helper {
+                public Order order(Member member, List<ProductOption> productOptions) {
+                    for (int i = 0; i < productOptions.size(); i++) {
+                        ProductOption productOption = productOptions.get(i);
+
+                        cartService.addItem(member, productOption, i + 1);
+                    }
+
+                    return orderService.createFromCart(member);
+                }
+            }
+
+            Helper helper = new Helper();
+
             String password = "{noop}1234";
             Member member1 = memberService.join("user1", password, "user1@test.com");
             Member member2 = memberService.join("user2", password, "user2@test.com");
@@ -37,8 +52,7 @@ public class DevInitData {
             // 5천원 사용
             memberService.addCash(member1, -5_000, "출금__일반");
 
-            // 삼십만원 충전
-            memberService.addCash(member1, 300_000, "충전__무통장입금");
+            memberService.addCash(member1, 1_000_000_000, "충전__무통장입금");
 
             // 현재 보유중인 캐시 금액
             long restCash = memberService.getRestCash(member1);
@@ -49,13 +63,16 @@ public class DevInitData {
             Product product2 = productService.create("쉬폰 OPS", 72000, 55000, "청평화 A-1-15", Arrays.asList(new ProductOption("BLACK", "44"), new ProductOption("BLACK", "55"), new ProductOption("WHITE", "44"), new ProductOption("WHITE", "55")));
 
             ProductOption product1Option__RED_44 = product1.getProductOptions().get(0);
+            ProductOption product1Option__RED_55 = product1.getProductOptions().get(1);
             ProductOption product1Option__BLUE_44 = product1.getProductOptions().get(2);
+            ProductOption product1Option__BLUE_55 = product1.getProductOptions().get(3);
 
-            cartService.addItem(member1, product1Option__RED_44, 1); // productOption__RED_44 총 수량 1
-            cartService.addItem(member1, product1Option__RED_44, 2); // productOption__RED_44 총 수량 3
-            cartService.addItem(member1, product1Option__BLUE_44, 1); // productOption__BLUE_44 총 수량 1
-
-            Order order1 = orderService.createFromCart(member1);
+            Order order1 = helper.order(member1, Arrays.asList(
+                            product1Option__RED_44,
+                            product1Option__RED_44,
+                            product1Option__BLUE_44
+                    )
+            );
 
             int order1PayPrice = order1.calculatePayPrice();
             orderService.payByRestCashOnly(order1);
@@ -65,21 +82,42 @@ public class DevInitData {
             // - 주문 생성
 
             ProductOption product2Option__BLACK_44 = product2.getProductOptions().get(0);
+            ProductOption product2Option__BLACK_55 = product2.getProductOptions().get(1);
             ProductOption product2Option__WHITE_44 = product2.getProductOptions().get(2);
+            ProductOption product2Option__WHITE_55 = product2.getProductOptions().get(3);
 
-            cartService.addItem(member2, product1Option__RED_44, 1);
-            cartService.addItem(member2, product2Option__BLACK_44, 2);
-            cartService.addItem(member2, product2Option__WHITE_44, 2);
-
-            Order order2 = orderService.createFromCart(member2);
+            Order order2 = helper.order(member2, Arrays.asList(
+                            product1Option__RED_44,
+                            product2Option__BLACK_44,
+                            product2Option__WHITE_44
+                    )
+            );
 
             log.debug("order2 payPrice : " + order2.calculatePayPrice());
 
-            memberService.addCash(member2, 370_000, "충전__무통장입금");
+            memberService.addCash(member2, 1_000_000_000, "충전__무통장입금");
 
             log.debug("member2 restCash : " + member2.getRestCash());
 
             orderService.payByRestCashOnly(order2);
+
+            orderService.refund(order2);
+
+            Order order3 = helper.order(member2, Arrays.asList(
+                            product1Option__RED_44,
+                            product1Option__RED_55,
+                            product2Option__BLACK_44,
+                            product2Option__WHITE_44
+                    )
+            );
+            orderService.payByRestCashOnly(order3);
+
+            Order order4 = helper.order(member1, Arrays.asList(
+                            product1Option__RED_44,
+                            product2Option__WHITE_44
+                    )
+            );
+            orderService.payByRestCashOnly(order4);
         };
     }
 }
