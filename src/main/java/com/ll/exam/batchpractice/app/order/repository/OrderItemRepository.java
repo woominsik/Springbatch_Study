978d8a17caf1f86b04c069791ff6eb22a85dfe02
woominsik <9737638@naver.com>
2022-10-03 23:37:25 +0900

    범위정산

--- src/main/java/com/ll/exam/batchpractice/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/batchpractice/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,10 @@
 package com.ll.exam.batchpractice.app.order.repository;
 
 import com.ll.exam.batchpractice.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
 }
