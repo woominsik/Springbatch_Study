a1d8e55efeafc0547e1fc8e7f0d02af9366fae38
woominsik <9737638@naver.com>
2022-10-04 11:13:56 +0900

    결제가 안된 주문품목은 정산데이터로 넘어가지 않는다.

--- src/main/java/com/ll/exam/batchpractice/app/order/repository/OrderRepository.java
+++ src/main/java/com/ll/exam/batchpractice/app/order/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.ll.exam.batchpractice.app.order.repository;
 
 import com.ll.exam.batchpractice.app.order.entity.Order;
+import com.ll.exam.batchpractice.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
+
+    Page<OrderItem> findAllByIdBetween(long fromId, long toId, Pageable pageable);
+
+    Page<OrderItem> findAllByIsPaid(boolean isPaid, Pageable pageable);
 }
